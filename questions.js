8596afa54f912dbf97cc46cba0941007d5f40c47
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2019-12-16 20:12:03 -0800

    initial commit, HTML, array of questions and answers and stylesheet

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "The # symbol specifies that the selector is?",
+        choices: ["class", "tags", "id", "css"],
+        answer: "id"
+    },
+    {
+        title: "Which HTML tag is used to define and internal style sheet?",
+        choices: ["<css>", "<body>", "<script>", "<style>"],
+        answer: "<style>"
+    },
+    {
+        title: "The coding languages Java and JavaScript are the same",
+        choices:["true", "false"],
+        answer: "false"
+    },
+    {
+        title: "An algorithm is:",
+        choices: ["A Dance", "A Selector in js", "A List of steps in order to complete a task", "None of the above"],
+        answer: "A list of steps in order to complete a task"
+    },
+    {
+        title: "What does the following expression return? '!false'",
+        choices:["null", "true", "undefined", "false"],
+        answer: "true"
+    },
+]
